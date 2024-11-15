8760f067e185307f81d8f2cc0cb7ebcd88ecbb21
heagenb03 <heagenbell@outlook.com>
2024-11-14 23:07:28 -0600

    modified:   add functions that fetch csv data to api

--- main.py
+++ main.py
@@ -5,3 +5,32 @@ class CarbonCalculator:
     def __init__(self):
         self.csv_manager = CSVManager()
         self.carbon_interface = CarbonInterface()
+        
+    def calculate_electricity_emissions(self):
+        data = self.csv_manager.electricity_data
+        emissions = []
+        for _, row in data.iterrows():
+            country = row['country']
+            state = row['state']
+            unit = row['electricity_unit']
+            value = row['electricity_value']
+            response = self.carbon_interface.estimate_eletricity(value, unit, country, state)
+            emissions.append(response)
+            
+        return emissions
+    
+    def calculate_flight_emissions(self):
+        data = self.csv_manager.flight_data
+        emissions = []
+        for _, row in data.iterrows():
+            passengers = row['passengers']
+            departure = row['departure_airport']
+            destination = row['destination_airport']
+            round_trip = True if row['round_trip'] == 'true' else False
+            response = self.carbon_interface.estimate_flight(passengers, departure, destination, round_trip)
+            emissions.append(response)
+            
+        return emissions
+    
+API = CarbonCalculator()
+print(API.calculate_flight_emissions())

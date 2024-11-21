ea76203b119d6fe4938a2b1aafeb3be9529695ca
heagenb03 <heagenbell@outlook.com>
2024-11-20 21:23:51 -0600

    modified:   insert electricity data to mysql db

--- mysqlmanager.py
+++ mysqlmanager.py
@@ -1,15 +1,29 @@
 import os
 import mysql.connector
 from dotenv import load_dotenv
+from calculatecarboon import CarbonCalculator
+
 load_dotenv()
+class MySQLManager:
+    def __init__(self):
+        self.mydb = mysql.connector.connect(
+            host='localhost',
+            user=os.getenv('MYSQL_USER'),
+            password=os.getenv('MYSQL_PASSWORD'),
+            database=os.getenv('MYSQL_DB')
+        )
+        self.mycursor = self.mydb.cursor()
+        
+        self.calculate_carbon = CarbonCalculator()
 
-mydb = mysql.connector.connect(
-    host='localhost',
-    user=os.getenv('MYSQL_USER'),
-    password=os.getenv('MYSQL_PASSWORD'),
-    database=os.getenv('MYSQL_DB')
-)
+    def insert_electricity_emissions(self):
+        emissions = self.calculate_carbon.calculate_electricity_emissions()
+        sql = 'INSERT INTO electricity (country, state, electricity_value, electricity_unit, carbon_emission) VALUES (%s, %s, %s, %s, %s)'
+        for emission in emissions:
+            val = (emission['country'], emission['state'], emission['value'], emission['unit'], emission['emission'])
+            self.mycursor.execute(sql, val)
+            
+        self.mydb.commit()
 
-mycursor = mydb.cursor()
-
-mycursor.execute('ALTER TABLE vehicle MODIFY COLUMN distance_value FLOAT')
+manager = MySQLManager()
+manager.insert_electricity_emissions()
